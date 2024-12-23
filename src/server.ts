f6e1282164e734170660f4e51f3c217f91a99b45
unknown <luan.santos@rspneus.com.br>
2024-12-23 20:34:24 -0300

    terceiro-commit

--- src/server.ts
+++ src/server.ts
@@ -3,7 +3,7 @@ import express from "express";
 const server = express();
 
 server.get("/", (req, res) => {
-  res.send("Boa tarde, mundo");
+  res.send("Boa noite, mundo");
 });
 
 server.listen(3000, () => {
