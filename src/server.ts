33e601198a04804852ad4b12e9c9938bbbbd57c1
unknown <luan.santos@rspneus.com.br>
2024-12-23 15:58:36 -0300

    segundo-commit

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
-import { createServer } from "node:http";
+import express from "express";
 
-const server = createServer((req, res) => {
-  res.end("Ola mundo");
+const server = express();
+
+server.get("/", (req, res) => {
+  res.send("Boa tarde, mundo");
 });
 
 server.listen(3000, () => {
-  console.log(`Servidor funcionando em http://localhost:3000`);
+  console.log("Servidor está rodando no link: http://localhost:3000/");
 });
