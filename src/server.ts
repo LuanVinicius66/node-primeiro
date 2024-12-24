176419c2c14365a7c8286b7b793e5ecf82a4da6c
unknown <luan.santos@rspneus.com.br>
2024-12-24 10:09:35 -0300

    Adcionado novas definições de servidor

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,19 @@
 import express from "express";
+import helmet from "helmet";
+import path from "path";
 
 const server = express();
 
+server.use(helmet());
+server.use(express.json());
+server.use(express.urlencoded({ extended: true }));
+server.use(express.static(path.join(__dirname, "../public")));
+
 server.get("/", (req, res) => {
-  res.send("Boa noite, mundo");
+  let name = "Luan";
+  let age = 24;
+
+  res.json({ name, age });
 });
 
 server.listen(3000, () => {
