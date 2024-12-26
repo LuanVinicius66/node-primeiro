ad60580efde90f345dcb272f6dcd433ffe4f65ea
unknown <luan.santos@rspneus.com.br>
2024-12-26 12:40:28 -0300

    Validações de erro de requisição

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,21 +1,21 @@
-import express from 'express';
-import produtosRouter from './produtos';
-import voosRouter from './voos';
+import express from "express";
+import produtosRouter from "./produtos";
+import voosRouter from "./voos";
 
 const router = express.Router();
 
-router.use('/produtos', produtosRouter);
-router.use('/voos', voosRouter);
+router.use("/produtos", produtosRouter);
+router.use("/voos", voosRouter);
 
-router.get('/ping', (req, res) => {
-    res.json({ pong: true});
-  });
-  
-  router.get("/", (req, res) => {
-    let name = "Luan";
-    let age = 24;
-  
-    res.json({ name, age });
-  });
+router.get("/ping", (req, res) => {
+  res.json({ pong: true });
+});
+
+router.get("/", (req, res) => {
+  let name = "Luan";
+  let age = 24;
+
+  res.json({ name, age });
+});
 
 export default router;
--- src/routes/voos.ts
+++ src/routes/voos.ts
@@ -1,17 +1,17 @@
-import express from 'express';
+import express from "express";
 
 const router = express.Router();
 
-router.get('/:from/:to', (req, res) => {
-    const { from, to } = req.params;
+router.get("/:from/:to", (req, res) => {
+  const { from, to } = req.params;
 
-    res.json({
-        flight: {
-            from: from.toUpperCase(),
-            to: to.toUpperCase(),
-            price: 45000
-        }
-    })
-})
+  res.json({
+    flight: {
+      from: from.toUpperCase(),
+      to: to.toUpperCase(),
+      price: 45000,
+    },
+  });
+});
 
 export default router;
